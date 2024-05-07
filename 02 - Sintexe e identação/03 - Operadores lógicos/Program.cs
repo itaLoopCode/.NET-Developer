647785db054b6786bd0ba81dc7e2c1eca51260ca
Italo Silva <italo_j.s@hotmail.com>
2024-05-07 13:59:27 -0300

    Operador OR

---
+++ 02 - Sintexe e identação/03 - Operadores lógicos/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Você é maior de idade? responda com sim ou não");
+string idade = Console.ReadLine();
+
+Console.WriteLine("Caso não seja, possui autorização dos seus responsáveis?");
+string autorizacao = Console.ReadLine();
+
+if (idade == "sim" || autorizacao == "sim")
+{
+    Console.WriteLine("Entrada liberada");
+}
+else
+{
+    Console.WriteLine("Entrada negada");
+}
+
+
+
+
+
+
+
+
+
+
+
+// bool maiorDeIdade = true;
+// bool autorizacaoDosResponsaveis = false;
+
+// Console.WriteLine("Você é maior de idade, responda sim ou não");
+// Console.ReadLine();
+
+// Console.WriteLine("Tem autorização dos responsáveis");
