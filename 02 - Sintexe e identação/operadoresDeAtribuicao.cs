1101787929ff5e9fefc6b6ff9e3b85e7ca2c8a12
Italo Silva <italo_j.s@hotmail.com>
2024-05-07 00:11:11 -0300

    committed

--- 02 - Sintexe e identação/operadoresDeAtribuicao.cs
+++ 02 - Sintexe e identação/operadoresDeAtribuicao.cs
@@ -1,8 +1,27 @@
-﻿using ExemploFundamentos.Modal;
-
+﻿//operador de atribuição
 int a = 10;
 int b = 20;
 
-int c = a+b;
-int d = a-b;
- Console.WriteLine("d", a, b, c );
+int c = a + b;
+
+Console.WriteLine(c);
+
+// combinando operadores
+
+c += 5;
+Console.WriteLine(c);
+c -= 6;
+Console.WriteLine(c);
+
+// convertendo tipos de variáveis,cast || casting
+
+a = Convert.ToInt32("5");
+Console.WriteLine(a);
+
+b = int.Parse("6");
+Console.WriteLine(b);
+
+// Diferença entre Parse e Convert
+
+// convert funcioca com valores null
+//parse não converte e fecha o programa para o usuário
