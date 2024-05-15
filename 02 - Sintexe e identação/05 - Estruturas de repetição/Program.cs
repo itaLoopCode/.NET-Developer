fa9bc942e2564d55a4dde95d0fbcad253b9ec9cb
Italo Silva <italo_j.s@hotmail.com>
2024-05-14 21:30:13 -0300

    commit

--- 02 - Sintexe e identação/05 - Estruturas de repetição/Program.cs
+++ 02 - Sintexe e identação/05 - Estruturas de repetição/Program.cs
@@ -1,12 +1,21 @@
-﻿int numero = 5;
+﻿ int numero = 5;
+ int contador = 0;
 
-Console.WriteLine($"{numero} x 1 = {numero * 1}");
+// Console.WriteLine($"{numero} x 1 = {numero * 1}");
 
 
-for (int contador = 0; contador <= 10; contador++){
+// for (int contador = 0; contador <= 10; contador++){
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
+
+// for (int contador = 0; contador <= 10; contador++) {
+//     Console.WriteLine($"{contador} + {numero} = {contador + numero}");
+// }
+
+//While
+
+while (contador <= 10)
+{
     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-}
-
-for (int contador = 0; contador <= 10; contador++) {
-    Console.WriteLine($"{contador} + {numero} = {contador + numero}");
+    contador++;
 }
--- 02 - Sintexe e identação/05 - Estruturas de repetição/obj/Debug/net8.0/05 - Estruturas de repetição.AssemblyInfo.cs
+++ 02 - Sintexe e identação/05 - Estruturas de repetição/obj/Debug/net8.0/05 - Estruturas de repetição.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("05 - Estruturas de repetição")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e06b177d9d2bd9c2c9e9a49018f594bbed1ca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6ec63512fb3c8e7abf11c8f62dd6504e2f7123")]
 [assembly: System.Reflection.AssemblyProductAttribute("05 - Estruturas de repetição")]
 [assembly: System.Reflection.AssemblyTitleAttribute("05 - Estruturas de repetição")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
